2dec13f3ea25f74ba2a6fe2f4f94e2a1b8717b20
Marcus Ogden <marcus@userlite.com>
2016-12-16 12:25:50 -0700

    finished

--- src/assessment.js
+++ src/assessment.js
@@ -10,10 +10,10 @@
 
 //         function son(){
 //             var playnintendo = "Mario";
-//         }   
+//         }
 //     }
 
-//     function uncle(){        
+//     function uncle(){
 //         var badjoke = "Har";
 
 //         function cousin(){
@@ -52,12 +52,26 @@ var foo;
 */
 
 
+function async(){
+  var defer = $q.defer();
+  setTimeout(function(){
+     foo = "bar";
+    defer.resolve();
+  }, 2000)
+
+  return defer.promise;
+}
+
+
+
 // #3  ###################
 // # Context 1
 // Write a function called context1 that takes in 4 parameters: A function called myFn, an object called context, param1, and param2.
 // Invoke myFn explicitly setting the context to the object called context.  Pass in param1 and param2 in order as well.
 
-
+function context1(myFn,context,param1, param2 ){
+  return myFn.call(context,param1,param2);
+}
 
 
 // #4  ###################
@@ -65,7 +79,9 @@ var foo;
 // Write a function called context2 that takes in 3 parameters: A function called myFn, an object called context, and an array called params
 // Invoke myFn explicitly setting the context to the object called context.  Pass in params
 
-
+function context2(myFn, context, params){
+  return myFn.apply(context, params)
+}
 
 
 // #5  ###################
@@ -73,55 +89,94 @@ var foo;
 // Write a function called context3 that takes in 2 parameters: A function called myFn, and an object called context
 // Make sure the function is permanently linked to the context.  This should give you a new function, return it.
 
-
+function context3(myFn,context ){
+  return myFn.bind(context)
+}
 
 
 // #6  ###################
 // # Constructor Function
 // Make a constructor function called taco that takes in 3 parameters: shell, meat, veggies and assigns them to identically named properties.
 
-
+function Taco(shell,meat, veggies ){
+  this.shell = shell;
+  this.meat = meat;
+  this.veggies = veggies;
+}
 
 
 // #7  ###################
 // # Implicit binding
 // Make a constructor function called burrito.  It has a property called percentLeft = 100.  It has a property called eat that is a function.  When eat is invoked it uses context to implicitly subract 25 from the percentLeft on the burrito.
-
+function Burrito(){
+  this.percentLeft = 100;
+  this.eat = function(){
+    this.percentLeft -= 25;
+  }
+}
 
 
 // #8  ###################
-// # Prototype 1 
+// # Prototype 1
 // Add prototype function to the array type that doubles the value of every item in the array
 
 
-
+Array.prototype.doubler = function() {
+    for (i = 0; i < this.length; i++) {
+         this[i] *= 2;
+    }
+    return this
+}
 
 // #9  ###################
 // # Prototype 2
 // Write a constructor function called chimichanga.  It has a property called percentLeft = 100.  It has a prototype function called eat.  When eat is invoked it uses context to implicitly subract 20 from the precentLeft on the chimichanga.
 
-
+function Chimichanga(){
+  this.percentLeft = 100;
+  this.eat = function(){
+    this.percentLeft -= 20;
+  }
+}
 
 
 // #10  ###################
 // # Closure 1
 // Write a function called sentence machine.  It takes in a parameter called partOne.  It returns a function called sentenceSmasher.
-// When sentenceSmasher is invoked it should take in a parameter called partTwo and return a new string that adds partOne and partTwo together.  
+// When sentenceSmasher is invoked it should take in a parameter called partTwo and return a new string that adds partOne and partTwo together.
 
+function sentenceMachine(partOne){
+
+  return function sentenceSmasher(partTwo){
+    return partOne + partTwo
+  }
+}
 
 
 // #11  ###################
 // # Closure 2
-// Write a function called subway.  It takes in one parameter called personName.  
+// Write a function called subway.  It takes in one parameter called personName.
 // It needs to keep track of what ingredients the person wants on their sandwhich.
-// It needs to do this by returning a function called addIngredient.  
-// When addIngredient is invoked it saves that ingredient with previously added ingredients and then returns an object that looks like this (Replace the parts wrapped in <> with correct data): 
+// It needs to do this by returning a function called addIngredient.
+// When addIngredient is invoked it saves that ingredient with previously added ingredients and then returns an object that looks like this (Replace the parts wrapped in <> with correct data):
 // ```
 // {
 //     orderPerson: <Person name goes here>,
 //     ingredients: <Array of ingredients goes here>
 // }
 // ```
+
+function subway(personName){
+
+
+  return function addIngredient(ingredient){
+    ingredientsArr = []
+    ingredientsArr.push(ingredient)
+    this.orderPerson = personName;
+    this.ingredients = ingredientsArr;
+    return this;
+}
+}
 
 
 
@@ -130,4 +185,24 @@ var foo;
 // Write a function that takes in 2 parameters.
 // If both parameters are the same type an the same value return "Exact match".
 // If both parameters have the same value but are different types return "Different types"
-// Otherwise return "Different values" 
+// Otherwise return "Different values"
+
+function compareValues(peram1, peram2){
+if(peram1 === peram2){
+  return "Exact match"
+}else if(peram1 == peram2){
+  return "Different types"
+}else{
+  return "Different values"
+}
+
+}
+
+
+
+
+
+
+
+
+//
